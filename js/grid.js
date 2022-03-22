2bcdb9c2d444405238f598fde0b804d6ad8e79cc
Clement Azalbert <clementazalbert04@gmail.com>
2022-03-22 17:42:47 +0100

    added Cell class to grid.js

--- js/grid.js
+++ js/grid.js
@@ -3,18 +3,41 @@ const CELL_SIZE = 20;
 const CELL_GAP = 2;
 
 export default class Grid {
+  /* private variable */
+  #cells
+
   constructor(gridElement) {
     gridElement.style.setProperty("--grid-size", GRID_SIZE);
     gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
     gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
-    createCellElements(gridElement);
+    this.#cells = createCellElements(gridElement).map((cellElement, index) => {
+      return new Cell(
+        cellElement,
+        index % GRID_SIZE,
+        Math.floor(index / GRID_SIZE)
+      );
+    });
+    console.log(this.cells);
+  }
+}
+
+class Cell {
+  /* private variables */
+  #cellElement
+  #x
+  #y
+
+  constructor(cellElement, x, y) {
+    this.#cellElement = cellElement;
+    this.#x = x;
+    this.#y = y;
   }
 }
 
 function createCellElements(gridElement) {
   const cells = [];
   for (let index = 0; index < GRID_SIZE * GRID_SIZE; index++) {
-    const cell = document.createElement("div")
+    const cell = document.createElement("div");
     cell.classList.add("cell")
     cells.push(cell)
     gridElement.append(cell)
