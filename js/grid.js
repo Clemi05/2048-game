46d3355100f32ea882320317abcef92722e0387f
Clement Azalbert <clementazalbert04@gmail.com>
2022-03-22 17:30:17 +0100

    added function createCellElements in grid.js

--- js/grid.js
+++ js/grid.js
@@ -1,5 +1,23 @@
+const GRID_SIZE = 4;
+const CELL_SIZE = 20;
+const CELL_GAP = 2;
+
 export default class Grid {
   constructor(gridElement) {
-
+    gridElement.style.setProperty("--grid-size", GRID_SIZE);
+    gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
+    gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
+    createCellElements(gridElement);
   }
+}
+
+function createCellElements(gridElement) {
+  const cells = [];
+  for (let index = 0; index < GRID_SIZE * GRID_SIZE; index++) {
+    const cell = document.createElement("div")
+    cell.classList.add("cell")
+    cells.push(cell)
+    gridElement.append(cell)
+  }
+  return cells
 }
--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ import Grid from "./grid.js";
 
 const gameBoard = document.getElementById("game-board");
 
-const grid = new Grid()
+const grid = new Grid(gameBoard);
