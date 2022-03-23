d28a706b1263f729c3e59252444e486c431c677e
Clement Azalbert <clementazalbert04@gmail.com>
2022-03-23 10:54:39 +0100

    added function setupInput and handleInput in script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,33 @@ const gameBoard = document.getElementById("game-board");
 const grid = new Grid(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
+setupInput();
+
+function setupInput() {
+  window.addEventListener("keydown", handleInput, { once: true })
+}
+
+function handleInput(event) {
+  console.log(event.key);
+  switch (event.key) {
+    case "ArrowUp":
+      moveUp();
+      break
+    case "ArrowDown":
+      moveDown();
+      break
+    case "ArrowLeft":
+      moveLeft();
+      break
+    case "ArrowRight":
+      moveRight();
+      break
+    default:
+      setupInput();
+      return
+  }
+
+
+
+  setupInput();
+}
