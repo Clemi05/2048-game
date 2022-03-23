f76f1bdf7d29f45104c8f491e5d2dac84f52a53c
Clement Azalbert <clementazalbert04@gmail.com>
2022-03-23 10:46:57 +0100

    added setter value code for background color and text color var calculation

--- js/Tile.js
+++ js/Tile.js
@@ -5,26 +5,37 @@ export default class Tile {
   #value
 
   constructor(tileContainer, value = Math.random() > 0.5 ? 2 : 4) {
-    this.#tileElement = document.createElement("div")
+    this.#tileElement = document.createElement("div");
     this.#tileElement.classList.add("tile")
     tileContainer.append(this.#tileElement)
-    this.value = value
+    this.value = value;
   }
 
   set value(v) {
-    this.#value = v
-    this.#tileElement.textContent = v
+    /* will change the value for the color style of the tile */
+    this.#value = v;
+    this.#tileElement.textContent = v;
+    const power = Math.log2(v);
+    const backgroundLightness = 100 - power * 9;
+    this.#tileElement.style.setProperty(
+      "--background-lightness",
+      `${backgroundLightness}%`
+    )
+    this.#tileElement.style.setProperty(
+      "--text-lightness",
+      `${backgroundLightness <= 50 ? 90 : 10}%`
+    )
   }
 
   set x(value) {
-    /* Will set the value of the x of the tile in the CSS */
-    this.#x = value
+    /* Will set the value of the x in the CSS */
+    this.#x = value;
     this.#tileElement.style.setProperty("--x", value)
   }
 
   set y(value) {
-    /* Will set the value of the y of the tile in the CSS */
-    this.#y = value
+    /* Will set the value of the y in the CSS */
+    this.#y = value;
     this.#tileElement.style.setProperty("--y", value)
 
   }
